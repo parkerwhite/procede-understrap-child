63648d40b436ca3416ce0afdd923d2ab5a4f66b5
Tim Spinks <tim@monkishtypist.com>
2018-09-25 11:00:45 -0700

    testing deploy

--- header.php
+++ header.php
@@ -31,7 +31,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="hfeed site" id="page">
 
-	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
