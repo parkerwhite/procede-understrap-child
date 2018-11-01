4d16e25dd8e8b5d24e8a68727db89c197a2a59c6
Tim Spinks <tim@monkishtypist.com>
2018-11-01 11:39:14 -0700

    bicubic interpolation and more csses

--- src/js/child-theme.js
+++ src/js/child-theme.js
@@ -76,3 +76,64 @@
 		});
 	});
 })(jQuery);
+
+
+
+//
+// https://github.com/sukhoi1, 25 Jan 2016
+// bicubic-img-interpolation v0.1.0 jQuery plugin
+
+jQuery.fn.bicubicImgInterpolation = function(settings) {
+	var self = this;
+	jQuery(self).each(function() {
+		if(this.tagName === "IMG") {
+			var src = jQuery(this).attr('src');
+			var imgW = this.width;
+			var imgH = this.height;
+			jQuery(this).after("<canvas style='display: none' width='" + imgW +  "' height='" + imgH + "'></canvas>");
+			var can = jQuery(this).next()[0];
+			var callback = drawHighResolutionImgThumbnail;
+			drawCanvas(can, imgW*6, imgH*6, src, callback, this, settings.crossOrigin);
+		}
+	});
+
+	function drawCanvas(can, imgW, imgH, src, callback, imgEl, crossOrigin) {
+
+		var ctx = can.getContext('2d');
+		var img = new Image();
+		if(crossOrigin) {
+			img.setAttribute('crossOrigin', 'anonymous'); //tainted canvases may not be exported chrome, ie will also throw security error
+		}
+
+		var w = imgW;
+		var h = imgH;
+
+		img.onload = function() {
+
+			// Step it down several times
+			var can2 = document.createElement('canvas');
+			can2.width = w;
+			can2.height = h;
+			var ctx2 = can2.getContext('2d');
+
+			// Draw it at 1/2 size 3 times (step down three times), making sure we don't overlap it
+			ctx2.drawImage(img, 0, 0, w/2, h/2);
+			ctx2.drawImage(can2, 0, 0, w/2, h/2, w/2, h/2, w/4, h/4);
+			ctx.drawImage(can2, w/2, h/2, w/4, h/4, 0, 0, w/6, h/6);
+			if(callback) {
+				callback(can, this.src, imgEl);
+			}
+		};
+
+		img.src = src;
+	};
+
+	function drawHighResolutionImgThumbnail(can, attrSrc, imgEl) {
+		jQuery(imgEl).attr('src', can.toDataURL("image/png"));
+		jQuery(imgEl).attr('data-src', attrSrc);
+	};
+};
+
+jQuery( document ).ready(function() {
+	jQuery('img').bicubicImgInterpolation({});
+});
