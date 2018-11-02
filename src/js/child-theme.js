96b5255b3de53cc573cf2838906b1c3584ab0445
Tim Spinks <tim@monkishtypist.com>
2018-11-01 18:33:00 -0700

    js carousel fix for tabs

--- src/js/child-theme.js
+++ src/js/child-theme.js
@@ -77,9 +77,8 @@
 	});
 
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-		console.log($(e.target).attr("aria-controls")); // newly activated tab
-		// e.relatedTarget // previous active tab
-
+		var t = $(e.target).attr("aria-controls");
+		setCarouselHeight($('#'+t).find(".carousel"));
 	});
 })(jQuery);
 
